1f08e4ae2ea17c7025d066a17402bef02f6886d0
Julio Molina Soler <j.molina@telenet.be>
2013-12-23 21:09:42 +0100

    Added logging capabilities to the command line template and postgres also added option for interactive, still long way until getting a nice class

--- commandline.py
+++ commandline.py
@@ -1,15 +1,27 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 import os
+import logging
 import argparse
 
-if __name__ == '__main__':
+def main():
     parser = argparse.ArgumentParser(description="Template to create command line python applicaions")
-    parser.add_argument("-v","--verbose",help="Example switch",action="store_true")
+    parser.add_argument("-d","--debug",help="Enable debugging",action="store_true")
     parser.add_argument("-s","--something",help="Regular parameter")
     arguments = parser.parse_args()
-    if arguments.verbose:
-        print "Got a verbosed"
-    print "Argument: " + arguments.something
+    # Logging section
+    if arguments.debug:
+        logging.basicConfig(format='%(asctime)s [%(levelname)s] [%(module)s::%(funcName)s] %(message)s', level=logging.DEBUG)
+    else:
+        logging.basicConfig(format='%(asctime)s [%(levelname)s] [%(module)s::%(funcName)s] %(message)s', level=logging.INFO)
+
+    logging.debug("Argument something =  %s",arguments.something)
+    actual_code()
+
+def actual_code():
+    logging.info("Starting %s",__file__)
+    logging.info("End %s",__file__)
+
+if __name__ == '__main__':
+    main()
--- postgres.py
+++ postgres.py
@@ -4,34 +4,33 @@
 import psycopg2
 import os
 import argparse
+import logging
 
 class DBPostgres:
     """Class to connect to a postgreSQL and register to recieve notifications
     """
     connection = None
     arguments = None
+    logger = logging.getLogger()
 
     def connect(self,host,dbname,user,password):
         """Method to stablish connection towards the database
         Usage: DBConnect.connect(host,database,user,password)
         """
         try:
-            if self.arguments.debug:
-                print "Getting connected to {}".format(self.arguments.database)
-                self.connection = psycopg2.connect(host=host,dbname=dbname,user=user,password=password)
-            if self.arguments.debug:
-                print "Successfully connected"
+            self.logger.debug("Getting connected to {}".format(self.arguments.database))
+            self.connection = psycopg2.connect(host=host,dbname=dbname,user=user,password=password)
+            self.logger.debug("Successfully connected")
 
         except psycopg2.DatabaseError, e:
-            print 'Error %s' % e
+            self.logger.error('Error %s' % e)
 
 
     def disconnect(self):
         """Method to disconnect from the database
         """
-        if self.arguments.debug:
-            print "Disconnecting from {}".format(self.arguments.database)
-
+        self.logger.debug("Disconnecting from {}".format(self.arguments.database))
+        
         if self.connection:
             self.connection.close()
 
@@ -40,8 +39,7 @@ class DBPostgres:
         (*) if we pass data as tuples then we will proceed with an insert with the values
         (*) if register is set to True,it will receive changes when something changes in the table.
         """
-        if self.arguments.debug:
-            print "Excuting query "
+        self.logger.debug("Excuting query %s",query)
             
         if register:
             pass
@@ -54,7 +52,7 @@ class DBPostgres:
                     ret = cursor.fetchall
                     print ret
                 except psycopg2.DatabaseError, e:
-                    print 'Error %s' % e
+                    self.logger.error('Error %s' % e)
 
         elif query:
             pass
@@ -66,16 +64,36 @@ class DBPostgres:
     def run(self, *args):
         """Excute the class for testing purposes
         """
-        parser = argparse.ArgumentParser(description="Module to connect and get updates from PostgreSQL")
-        parser.add_argument("-d","--debug",help="Display the debug messages", action="store_true")
-        parser.add_argument("-i","--interactive",help="Enter in interactive mode to execute some SQL and wait for events",action="store_true")
-        parser.add_argument("username", help="Username to connect to the postgreSQL")
-        parser.add_argument("password", help="Password to connect to the postgreSQL")
-        parser.add_argument("hostname", help="Hostname of the postgreSQL")
-        parser.add_argument("database", help="Database name to connect")
-        self.arguments = parser.parse_args()
-        self.connect(self.arguments.hostname,self.arguments.database,self.arguments.username,self.arguments.password)
-        self.disconnect()
+        if __name__ == '__main__':
+            parser = argparse.ArgumentParser(description="Module to connect and get updates from PostgreSQL")
+            parser.add_argument("-v","--debug",help="Display the debug messages", action="store_true")
+            parser.add_argument("-i","--interactive",help="Enter in interactive mode to execute some SQL and wait for events",action="store_true")
+            parser.add_argument("-u","--username", help="Username to connect to the postgreSQL")
+            parser.add_argument("-p","--password", help="Password to connect to the postgreSQL")
+            parser.add_argument("-s","--server", help="Hostname of the postgreSQL")
+            parser.add_argument("-d","--database", help="Database name to connect")
+            self.arguments = parser.parse_args()
+            if self.arguments.debug:
+                logging.basicConfig(format='%(asctime)s [%(levelname)s] [%(module)s::%(funcName)s] %(message)s', level=logging.DEBUG)
+            else:
+                logging.basicConfig(format='%(asctime)s [%(levelname)s] [%(module)s::%(funcName)s] %(message)s', level=logging.INFO)
+            
+            self.logger.info("Starting %s",__file__)
+            self.logger.info("Requesting connection to database %s",self.arguments.database)
+            self.connect(self.arguments.server,self.arguments.database,self.arguments.username,self.arguments.password)
+            if self.arguments.interactive:
+                self.interactive()
+            self.logger.info("End %s",__file__)
+            self.disconnect()
+
+    def interactive(self):
+        self.logger.info("Entered in interactive mode")
+        imsg = raw_input('# ')
+        self.logger.info("Input [%s]",imsg)
+        while imsg <> 'exit':
+            imsg = raw_input('# ')
+            self.logger.info("Input [%s]",imsg)
+        self.logger.info("Exit from interactive mode")
 
 if __name__ == '__main__':
     db = DBPostgres()
