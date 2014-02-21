81e1db3e6575e962b2730471805ffbb6d6153236
Julio Molina Soler <j.molina@telenet.be>
2014-02-21 15:45:15 +0100

    First try to recognize the commands

--- commandline.lisp
+++ commandline.lisp
@@ -1,5 +1,20 @@
 ;;;; commandline.lisp
+;To have into considerationg about getopt
+; define if the parameter is unique
+; define if the parameter has one value
+; define if the paramater has some values
+(load "~/.clisprc.lisp")
 
+(defun findoption (command args)
+	"Check whether the command is in the arguments"
+	(cond 
+		((null args) nil)
+		((equal command (car args)) t)
+		((consp (car args)) (or (findoption command (car args))
+								(findoption command (cdr args))))
+		(t (findoption command (cdr args)))))
+
+(format t  "~S~%" (findoption "b" *args*))
 
 
 ;;; here comes commandline.
