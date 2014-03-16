d9bc259bea764c683968d6163cb84e0087116c9a
Julio Molina Soler <j.molina@telenet.be>
2014-03-16 18:00:45 +0100

    Update command-ref.sh

--- command-ref.sh
+++ command-ref.sh
@@ -1,6 +1,7 @@
 # Commands reference
 
 # compiling emacs
+apt-get install libxaw7-dev libxpm-dev libpng12-dev libtiff5-dev libgif-dev libjpeg8-dev libgtk2.0-dev libncurses5-dev
 # without x
 ./configure --withou-x
 
